75005c7c9f6f7a8cacefd3af68dc5eb058bd9731
Matteo Meneghetti <matteo@meneghetti.dev>
2023-08-09 09:44:32 +0200

    Included README, example usage and docstrings

--- src/QuadratureEncoder.cpp
+++ src/QuadratureEncoder.cpp
@@ -7,19 +7,48 @@ QuadratureEncoder::QuadratureEncoder(uint8_t pin, float ppr, float gear_ratio) :
     if (sm == -1)
         pio_instance = pio1;
     sm = pio_claim_unused_sm(pio_instance, true);
+    if (sm == -1)
+        throw std::runtime_error("No free state machine found!");
     uint offset = pio_add_program(pio_instance, &quadrature_encoder_program);
     quadrature_encoder_program_init(pio_instance, sm, pin, 0);
 }
 
-int32_t QuadratureEncoder::get_count()
+void QuadratureEncoder::update(float dt)
 {
-    return quadrature_encoder_get_count(pio_instance, sm);
+    _last_count = _count;
+    _count = quadrature_encoder_get_count(pio_instance, sm);
+    _dt = dt;
 }
 
-float QuadratureEncoder::get_angle(bool degrees)
+int32_t QuadratureEncoder::get_count()
+{
+    return _count;
+}
+
+float QuadratureEncoder::get_position(bool degrees)
 {
     if (degrees)
-        return 360.0f * this->get_count() / (ppr * gear_ratio);
+        return 360.0f * _count / (ppr * gear_ratio); // deg
     else
-        return 2 * M_PI * this->get_count() / (ppr * gear_ratio);
+        return 2 * M_PI * _count / (ppr * gear_ratio); // rad
+}
+
+float QuadratureEncoder::get_velocity(bool degrees)
+{
+    float velocity = ((_count - _last_count) / (ppr * gear_ratio)) / _dt;
+
+    if (degrees)
+        return 360.0f * velocity; // deg/s
+    else
+        return 2 * M_PI * velocity; // rad/s
+}
+
+bool QuadratureEncoder::get_direction()
+{
+    return _count > _last_count ? 1 : -1;
+}
+
+int8_t QuadratureEncoder::get_sign()
+{
+    return (_count > 0) - (_count < 0);
 }
